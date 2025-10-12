58e67563b2fd821e695f791a2c8222af295d183d
yacook1e <151845245+yacook1e@users.noreply.github.com>
2025-10-13 01:34:31 +0300

    Add files via upload

---
+++ lab2BD/Create_tables.sql
@@ -0,0 +1,44 @@
+CREATE TABLE Doctor (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    office_number INT,
+    full_name NVARCHAR(50) NOT NULL,
+    phone NVARCHAR(20),
+    speciality NVARCHAR(30)
+);
+
+CREATE TABLE District (
+    id INT IDENTITY(1,1) PRIMARY KEY ,
+    number INT NOT NULL,
+    doctor_id INT REFERENCES Doctor(id) ON DELETE SET NULL
+);
+
+CREATE TABLE Schedule (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    day_of_the_week NVARCHAR(15) NOT NULL,
+    start_time TIME NOT NULL,
+    duration INT NOT NULL,
+    doctor_id INT REFERENCES Doctor(id) ON DELETE CASCADE
+);
+
+CREATE TABLE Patient (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    phone NVARCHAR(20),
+    full_name NVARCHAR(50) NOT NULL,
+    district_Id INT REFERENCES District(id) ON DELETE SET NULL
+);
+
+CREATE TABLE Reception (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    reception_time DATETIME NOT NULL,
+    diagnosis NVARCHAR(100),
+    patient_id INT REFERENCES Patient(id) ON DELETE CASCADE,
+    doctor_id INT REFERENCES Doctor(id) ON DELETE NO ACTION
+);
+
+CREATE TABLE Stattalon (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    purpose NVARCHAR(100),
+    schedule_id INT REFERENCES Schedule(id) ON DELETE CASCADE,
+    doctor_id INT REFERENCES Doctor(id) ON DELETE NO ACTION,
+    patient_id INT REFERENCES Patient(id) ON DELETE NO ACTION
+);
